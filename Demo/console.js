2b2699678c9bd90516ec2b9716198ec8a9890713
吾爱学习 <57586633+GitACzero@users.noreply.github.com>
2019-11-10 23:31:21 +0800

    Update console.js

--- Demo/console.js
+++ Demo/console.js
@@ -8,6 +8,12 @@ console.log("调整位置...");
 console.setPosition(0, 500);
 sleep(2000);
 
-console.clear();//清空控制台。
+//文本显示的颜色
+console.verbose("这是灰色");
+console.log("这是白色");
+console.info("这是绿色");
+console.warn("这是蓝色");
+console.error("这是红色");
 
+console.clear();//清空控制台。
 console.hide();//隐藏控制台
