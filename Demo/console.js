4b3e17a5981dc17420ea72e5e351613b2a19fbda
吾爱学习 <57586633+GitACzero@users.noreply.github.com>
2019-11-10 23:21:16 +0800

    Create console.js
    控制台显示隐藏等相关

---
+++ Demo/console.js
@@ -0,0 +1,11 @@
+console.show();//显示控制台
+
+console.log("调整大小...");
+console.setSize(1000, 1000);
+sleep(2000);
+
+console.log("调整位置...");
+console.setPosition(0, 500);
+sleep(2000);
+
+console.hide();//隐藏控制台
