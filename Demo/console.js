2cf4c5d5e3343e80c0cd959541f7ed02e863304d
吾爱学习 <57586633+GitACzero@users.noreply.github.com>
2019-11-10 23:35:36 +0800

    Update console.js

--- Demo/console.js
+++ Demo/console.js
@@ -15,5 +15,13 @@ console.info("这是绿色");
 console.warn("这是蓝色");
 console.error("这是红色");
 
+//格式化输出
+var i = {
+    name: "小明",
+    age: 18,
+    height: 1.72
+};
+console.log("大家好, 我叫%s, 今年%d岁, 身高%d米", i.name, i.age, i.height);
+
 console.clear();//清空控制台。
 console.hide();//隐藏控制台
