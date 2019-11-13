3f71aafb26a3ebc562a6f46b4c813be409990204
ASUS <2296582473@qq.com>
2019-11-13 18:50:05 +0800

    将延时函数具体化，将秒和分钟分成两个函数

--- Projects/xxqg.js
+++ Projects/xxqg.js
@@ -16,122 +16,141 @@ toast("屏幕宽度为" + device.width + "  " + "屏幕高度为" + device.heigh
 
 var 学习强国 = "cn.xuexi.android";
 
+main();
+
 function initial() {
     auto.waitFor(); //等待获取无障碍辅助权限
+    console.setSize(1000, 500);
     console.show(); //显示控制台
-    console.setPosition(0, device.height / 4);
-
-    console.log("本脚本仅供学习测试...");
+   // console.setPosition(0, device.height / 4);
+    console.log("学习强国脚本启动中...");
+    delay_s(3);
     console.log("作者：Zero");
+    delay_s(2);
     console.log("版本：V1.0");
-    delay(3);
-
+    delay_s(2);
 }
 
 function loadApp() {
     console.log("运行：学习强国");
-    delay(3);
+    delay_s(3);
     log("准备：学习强国主界面..");
     launch(学习强国);
-    delay(6);
+    delay_s(6);
 }
 
 function task_video() {
-
     console.log("任务1：视听学习  3秒后开始...");
+    delay_s(3);
     click("百灵");
-    sleep(3000);
+    delay_s(5);
     百灵();
     console.log("进度：视听浏览任务完成，正在返回...");
-}
-
-function task_article() {
-    console.log("任务2：阅读文章  3秒后开始...");
-    //bounds(918 , 1818, 1026, 1890).clickable().click();
-    bounds(432, 1762, 648, 1920).clickable().click(); //click("学习");
-    sleep(3000);
-    学习();
-    console.log("进度：文章浏览任务完成，正在返回...");
-}
-
-function 学习() {
-    console.log("提示：进入学习...");
-    delay(3);
-    click("新思想");
-    delay(3);
-    click("综合");
-
-    delay(3);
-    for (let i = 1; i < 8; i++) {
-        console.log("进度：这是第" + i + "篇文章");
-        click(device.width / 2, 2 * device.height / 5, device.width / 2, device.height / 4);
-        delay(5);
-        if (text("学习进行时").exists()) {
-            console.log("警告：我要返回...");
-            delay(5);
-            back();
-            delay(6);
-            back();
-        }
-        swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, random() * delay(1));
-        delay(9);
-        if (i < 3) {
-            star();
-            share();
-            delay(5);
-        }
-        back();
-        delay(3);
-        swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, delay(4.5));
-    }
+    back();
 }
 
 function 百灵() {
-    sleep(3000); //缓冲3秒
-    console.log("提示：进入百灵，每个视频浏览时长8秒...");
+    delay_s(3); //缓冲3秒
+    console.log("提示：进入百灵主界面，每个视频浏览时长8秒...");
 
     //点击一个视频进入
     className("android.widget.ImageView").findOne().parent().click(); //找ImageView的上一层FrameLayout
     var video1 = className("android.widget.FrameLayout").findOne().bounds();
     click(device.width = 501, video1.centerY());
 
-    sleep(8000);
+    delay_s(8);
     click("继续播放");
 
     for (let i = 1; i < 7; i++) {
-        console.log("提示：这是第" + i + "个视频");
-        sleep(9000); //播放时间
-        scroll();
+        delay_s(5);
+        console.log("进度：完成" + i + "个视频浏览");
+        delay_s(9);//播放时间
+        scroll_it();
     }
 
     console.log("进度：最后一个视频持续12分钟...");
-    sleep(60000 * 6); //总时长6分钟
+    delay_m(1); //总时长6分钟
 }
 
+function task_article() {
+    console.log("任务2：阅读文章  3秒后开始...");
+    //bounds(918 , 1818, 1026, 1890).clickable().click();
+    bounds(432, 1762, 648, 1920).clickable().click(); //click("学习");
+    delay_s(3);
+    学习();
+    console.log("进度：文章浏览任务完成，正在返回...");
+    back();
+}
+
+function 学习() {
+    console.log("提示：进入学习主界面...");
+    delay_s(3);
+    click("新思想");
+    delay_s(3);
+    click("综合");
+
+    delay_s(3);
+    for (let i = 1; i < 8; i++) {
+        console.log("进度：进行第" + i + "次文章浏览");
+        click(device.width / 2, 2 * device.height / 5, device.width / 2, device.height / 4);
+        delay_s(5);
+
+        //判断是否进入强国号：新华社
+        if (text("学习进行时").exists()) {
+            console.log("警告：我要返回...");
+            delay_s(5);
+            back();
+            delay_s(9);
+            swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, 500);
+            delay_s(9);
+            click(device.width / 2, 2 * device.height / 5, device.width / 2, device.height / 4);
+        }
+        swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, random() * 1000);
+        delay_s(9);
+        if (i > 5) {
+            star();
+            delay_s(5);
+            share();
+            delay_s(5);
+        }
+        back();
+        delay_s(3);
+        swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, 4500);
+        console.log("进度：完成" + i + "次文章浏览");
+    }
+}
+
+
+
 //滚屏
-function scroll() {
-    swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, delay(1));
+function scroll_it() {
+    swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, 1000);
 }
 
 //延迟
-function delay(seconds) {
+function delay_s(seconds) {
     sleep(1000 * seconds);
+}
+function delay_m(minute) {
+    sleep(60000 * minute);
 }
 
 //分享
 function share() {
+    console.log("进度：分享...");
 
     bounds(918, 1818, 1026, 1890).clickable().click();
-    delay(3);
+    delay_s(3);
     click("分享到学习强国");
-    delay(2);
-    click("你要转发的名字或者群名"); //自行修改
-    delay(1);
+    delay_s(2);
+    click("李嘉龙"); //自行修改
+    delay_s(1);
     click("发送");
 }
 
 //收藏
 function star() {
+    console.log("进度：收藏...");
     bounds(774, 1818, 918, 1890).clickable().click();
 
 }
@@ -139,18 +158,9 @@ function star() {
 function main() {
     initial();
     loadApp(); //1.打开学习强国App
-    delay(3); //2.等待进入主页
-
-    task_video(); //3.任务1：视听学习 
-    back();
-    delay(3);
-
-    task_article(); //4.任务2：文章学习
-    back();
-    delay(3);
+    //task_video(); //2.任务1：视听学习 
+    task_article(); //3.任务2：文章学习
 
     console.hide(); //隐藏控制台
     exists(); //退出应用
 }
-
-main();
