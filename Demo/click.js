f17c73715a80d5a515a6c85aed7e7cc69d592b17
ASUS <2296582473@qq.com>
2019-11-14 10:20:06 +0800

    移动

--- Demo/click.js
+++
@@ -1,8 +0,0 @@
-//1.适用点击文字，页面存在的文本
-//click("全部播放",1);
-
-//2.适用点击图片，对应的精确坐标
-//bounds(918 , 1818, 1026, 1890).clickable().click();
-
-//3.按设备尺寸点击
-//click(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5);
